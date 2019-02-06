09fd9ad331fa1546d263ae23b00e1e44f73c5104
Darren <darren@darrenwhitlen.com>
2019-02-06 17:03:14 +0000

    Refactoring hooks into their own file. Easier to manage

--- src/worker/clientcommands.js
+++ src/worker/clientcommands.js
@@ -5,6 +5,9 @@ const { mParam, mParamU } = require('../libs/helpers');
 let commands = Object.create(null);
 let commandHooks = new EventEmitter();
 
+// Attach all the command hooks
+require('./clienthooks').hooks.forEach(hook => hook(commandHooks));
+
 module.exports.triggerHook = function triggerHook(hookName, event) {
     commandHooks.emit(hookName, event);
 };
@@ -107,147 +110,6 @@ async function maybeProcessRegistration(con) {
 
     await con.state.tempSet('reg.state', null);
 }
-
-
-/**
- * Tap into some hooks to modify messages and capabilities
- */
-
-// Some caps to always request
-commandHooks.on('available_caps', event => {
-    event.caps.push('batch');
-});
-
-// server-time support
-commandHooks.on('message_to_client', event => {
-    let caps = event.client.state.caps;
-
-    if (caps.includes('server-time')) {
-        if (!event.message.tags['time']) {
-            event.message.tags['time'] = strftime('%Y-%m-%dT%H:%M:%S.%LZ');
-        }
-    } else {
-        delete event.message.tags['time'];
-    }
-});
-commandHooks.on('available_caps', event => {
-    let caps = event.caps.push('server-time');
-});
-
-// away-notify support
-commandHooks.on('message_to_client', event => {
-    if (!event.client.state.caps.includes('away-notify') && event.message.command === 'AWAY') {
-        event.halt = true;
-    }
-});
-commandHooks.on('available_caps', event => {
-    event.caps.push('away-notify');
-});
-
-// account-notify support
-commandHooks.on('message_to_client', event => {
-    if (!event.client.state.caps.includes('account-notify') && event.message.command === 'ACCOUNT') {
-        event.halt = true;
-    }
-});
-commandHooks.on('available_caps', event => {
-    event.caps.push('account-notify');
-});
-
-// extended-join
-commandHooks.on('available_caps', event => {
-    if (!event.client.upstream) {
-        return;
-    }
-
-    // Only allow the client to use extended-join if upstream has it
-    let upstream = event.client.upstream;
-    if (upstream.state.caps.include('extended-join')) {
-        event.caps.push('extended-join');
-    }
-});
-commandHooks.on('message_to_client', event => {
-    // :nick!user@host JOIN #channelname * :Real Name
-    let caps = event.client.state.caps;
-    let m = event.message;
-    if (!caps.includes('extended-join') && m.command === 'JOIN' && m.params.length > 2) {
-        // Drop the account name from the params (The * in the above example)
-        m.params.splice(1, 1);
-    }
-});
-
-// multi-prefix
-commandHooks.on('available_caps', event => {
-    event.caps.push('multi-prefix');
-});
-commandHooks.on('message_to_client', event => {
-    let m = event.message;
-    // Only listen for 353(NAMES) and 352(WHO) replies
-    if (m.command !== '353' && m.command !== '352') {
-        return;
-    }
-
-    if (!event.client.upstream) {
-        return;
-    }
-
-    let clientCaps = event.client.state.caps;
-    let upstreamCaps = event.client.upstream.state.caps;
-    if (!clientCaps.includes('multi-prefix') && upstreamCaps.includes('multi-prefix')) {
-        // Make sure only one prefix is included in the message before sending them to the client
-
-        let prefixes = event.client.upstream.state.isupports.find(token => {
-            return token.indexOf('PREFIX=') === 0;
-        });
-
-        // Convert "PREFIX=(qaohv)~&@%+" to "~&@%+"
-        prefixes = (prefixes || '').split('=')[1] || '';
-        prefixes = prefixes.substr(prefixes.indexOf(')') + 1);
-
-        // :server.com 353 guest = #tethys :~&@%+aji &@Attila @+alyx +KindOne Argure
-        if (m.command === '353') {
-            // Only keep the first prefix for each user from the userlist
-            let list = m.params[3].split(' ').map(item => {
-                let itemPrefixes = '';
-                let nick = '';
-                for (let i = 0; i < item.length; i++) {
-                    if (prefixes.indexOf(item[i]) > -1) {
-                        itemPrefixes += item[i];
-                    } else {
-                        nick = item.substr(i + 1);
-                        break;
-                    }
-                }
-
-                return itemPrefixes[0] + nick;
-            });
-
-            m.params[3] = list.join(' ');
-        }
-
-        // :kenny.chatspike.net 352 guest #test grawity broken.symlink *.chatspike.net grawity H@%+ :0 Mantas M.
-        if (m.command === '352') {
-            let remapped = '';
-            let status = m.params[6] || '';
-            if (status[0] === 'H' || status[0] === 'A') {
-                remapped += status[0];
-                status = status.substr(1);
-            }
-
-            if (status[0] === '*') {
-                remapped += status[0];
-                status = status.substr(1);
-            }
-
-            if (status[0]) {
-                remapped += status[0];
-                status = status.substr(1);
-            }
-
-            m.params[6] = remapped;
-        }
-    }
-});
 
 
 /**
