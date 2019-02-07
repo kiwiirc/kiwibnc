e753479af908bbb19e4266ace10f687f59c61821
Darren <darren@darrenwhitlen.com>
2019-02-07 12:01:51 +0000

    Using knexjs instead of sqlite3 directly

--- src/libs/database.js
+++ src/libs/database.js
@@ -1,44 +1,26 @@
-const sqlite3 = require('sqlite3');
+const knex = require('knex');
 
 module.exports = class Database {
     constructor(dbPath) {
-        this.db = new sqlite3.Database(dbPath);
+		this.db = knex({
+			client: 'sqlite3',
+			connection: {
+				filename: dbPath,
+			},
+			useNullAsDefault: true,
+        });
     }
 
     get(sql, params) {
-        return new Promise((resolve, reject) => {
-            this.db.get(sql, params, (err, row) => {
-                if (err) {
-                    reject(err);
-                }
-
-                resolve(row);
-            });
-        });
+        return this.db.raw(sql, params).then(rows => rows[0]);
     }
 
     all(sql, params) {
-        return new Promise((resolve, reject) => {
-            this.db.all(sql, params, (err, rows) => {
-                if (err) {
-                    reject(err);
-                }
-
-                resolve(rows);
-            });
-        });
+        return this.db.raw(sql, params);
     }
 
     run(sql, params) {
-        return new Promise((resolve, reject) => {
-            this.db.run(sql, params, (err) => {
-                if (err) {
-                    reject(err);
-                }
-
-                resolve();
-            });
-        });
+        return this.db.raw(sql, params);
     }
 
     async init() {
