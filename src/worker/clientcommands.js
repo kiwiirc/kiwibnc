51d26b9061575cefb02a1535690adc33d8fdba77
Darren <darren@darrenwhitlen.com>
2019-02-12 11:25:39 +0000

    Majority of the bouncer commands implemented

--- src/worker/clientcommands.js
+++ src/worker/clientcommands.js
@@ -257,6 +257,147 @@ commands.QUIT = async function(msg, con) {
     return false;
 };
 
+commands.BOUNCER = async function(msg, con) {
+    let subCmd = mParamU(msg, 0, '');
+
+    let encodeTags = require('irc-framework/src/messagetags').encode;
+
+    if (subCmd === 'CONNECT') {
+        let netName = mParam(msg, 1, '');
+        if (!netName) {
+            con.writeMsg('BOUNCER', 'connect', '*', 'ERR_INVALIDARGS');
+            return false;
+        }
+
+        let network = await con.userDb.getNetworkByName(con.state.authUserId, netName);
+        if (!network) {
+            con.writeMsg('BOUNCER', 'connect', '*', 'ERR_NETNOTFOUND');
+            return false;
+        }
+
+        let upstream = null;
+        upstream = con.conDict.findUsersOutgoingConnection(con.state.authUserId, network.id);
+        if (upstream && !upstream.state.connected) {
+            upstream.open();
+        } else if(!upstream) {
+            upstream = await con.makeUpstream(network);
+        }
+    }
+
+    if (subCmd === 'DISCONNECT') {
+        let netName = mParam(msg, 1, '');
+        if (!netName) {
+            con.writeMsg('BOUNCER', 'disconnect', '*', 'ERR_INVALIDARGS');
+            return false;
+        }
+
+        let network = await con.userDb.getNetworkByName(con.state.authUserId, netName);
+        if (!network) {
+            con.writeMsg('BOUNCER', 'disconnect', '*', 'ERR_NETNOTFOUND');
+            return false;
+        }
+
+        let upstream = null;
+        upstream = con.conDict.findUsersOutgoingConnection(con.state.authUserId, network.id);
+        if (upstream && upstream.state.connected) {
+            upstream.close();
+        }
+    }
+
+    if (subCmd === 'LISTNETWORKS') {
+        let nets = await con.userDb.getUserNetworks(con.state.authUserId);
+        nets.forEach((net) => {
+            let parts = [];
+            parts.push('network=' + net.name);
+            parts.push('host=' + net.host);
+            parts.push('port=' + net.port);
+            parts.push('tls=' + net.tls ? '1' : '0');
+            parts.push('host=' + net.host);
+
+            let netCon = con.conDict.findUsersOutgoingConnection(con.state.authUserId, net.id);
+            if (netCon) {
+                parts.push('state=' + (netCon.state.connected ? 'connected' : 'disconnected'));
+            } else {
+                parts.push('state=disconnect');
+            }
+
+            con.writeMsg('BOUNCER', 'listnetworks', parts.join(';'));
+        });
+
+        con.writeMsg('BOUNCER', 'listnetwork', 'RPL_OK');
+    }
+
+    if (subCmd === 'LISTBUFFERS') {
+        let netName = mParam(msg, 1, '');
+        if (!netName) {
+            con.writeMsg('BOUNCER', 'listbuffers', '*', 'ERR_INVALIDARGS');
+            return false;
+        }
+
+        let network = await con.userDb.getNetworkByName(con.state.authUserId, netName);
+        if (!network) {
+            con.writeMsg('BOUNCER', 'listbuffers', '*', 'ERR_NETNOTFOUND');
+            return false;
+        }
+
+        let upstream = null;
+        upstream = con.conDict.findUsersOutgoingConnection(con.state.authUserId, network.id);
+        if (upstream) {
+            for (let chanName in upstream.state.channels) {
+                let buffer = upstream.state.channels[chanName];
+                let chan = {
+                    network: network.name,
+                    buffer: buffer.name,
+                    joined: buffer.joined ? '1' : '0',
+                    topic: buffer.topic,
+                };
+                con.writeMsg('BOUNCER', 'listbuffers', network.name, encodeTags(chan));
+            }
+        }
+
+        con.writeMsg('BOUNCER', 'listbuffers', network.name, 'RPL_OK');
+    }
+
+    if (subCmd === 'DELBUFFER') {
+        let netName = mParam(msg, 1, '');
+        let bufferName = mParam(msg, 2, '');
+        if (!netName || !bufferName) {
+            con.writeMsg('BOUNCER', 'delbuffer', '*', 'ERR_INVALIDARGS');
+            return false;
+        }
+
+        let network = await con.userDb.getNetworkByName(con.state.authUserId, netName);
+        if (!network) {
+            con.writeMsg('BOUNCER', 'delbuffer', '*', 'ERR_NETNOTFOUND');
+            return false;
+        }
+
+        let upstream = null;
+        upstream = con.conDict.findUsersOutgoingConnection(con.state.authUserId, network.id);
+        if (!upstream) {
+            // TODO: If no upstream loaded, check if its in the db (network) and remove it from there
+            con.writeMsg('BOUNCER', 'delbuffer', network.name, bufferName, 'RPL_OK');
+            return false;
+        }
+
+        
+        let buffer = upstream.state.getChannel(bufferName);
+        if (!buffer) {
+            // No buffer? No need to delete anything
+            con.writeMsg('BOUNCER', 'delbuffer', network.name, bufferName, 'RPL_OK');
+        }
+
+        upstream.state.delChannel(buffer.name);
+        if (buffer.joined) {
+            upstream.writeLine('PART', buffer.name);
+        }
+
+        con.writeMsg('BOUNCER', 'delbuffer', network.name, bufferName, 'RPL_OK');
+    }
+
+    return false;
+};
+
 // TODO: Put these below commands behind a login or something
 commands.KILL = async function(msg, con) {
     con.queue.stopListening().then(process.exit);
