6b1daa62fe24b443ca69e5f82c4eabdfb69cdfb4
Darren <darren@darrenwhitlen.com>
2019-12-01 20:02:17 +0000

    Admin UI adding new users

--- src/worker/extensions/kiwiweb/routes_admin.js
+++ src/worker/extensions/kiwiweb/routes_admin.js
@@ -22,7 +22,8 @@ module.exports = function(app) {
             .as('num_networks');
 
         let users = await db('users')
-            .select('id', 'username', 'created_at', 'admin', 'locked', r);
+            .select('id', 'username', 'created_at', 'admin', 'locked', r)
+            .orderBy(['admin', 'username']);
 
         ctx.body = {
             started,
@@ -42,24 +43,32 @@ module.exports = function(app) {
     router.post('admin.users', '/api/admin/users', async (ctx, next) => {
         let body = ctx.request.body;
 
-        let acts = ['lock', 'unlock', 'changepass'];
+        let acts = ['lock', 'unlock', 'changepass', 'newuser'];
         if (!acts.includes(body.act)) {
             ctx.body = {error: 'unknown_act' };
             return;
         }
 
-        let user = await userDb.getUser(body.username);
-        if (!user) {
-            ctx.body = {error: 'no_user' };
-            return;
-        }
-
         if (body.act === 'lock') {
+            let user = await userDb.getUser(body.username);
+            if (!user) {
+                ctx.body = {error: 'no_user' };
+                return;
+            }
+
             user.locked = true;
             await user.save();
+
         } else if (body.act === 'unlock') {
+            let user = await userDb.getUser(body.username);
+            if (!user) {
+                ctx.body = {error: 'no_user' };
+                return;
+            }
+
             user.locked = false;
             await user.save();
+
         } else if (body.act === 'changepass') {
             let newPass = body.password;
             if (!newPass) {
@@ -67,8 +76,38 @@ module.exports = function(app) {
                 return;
             }
 
+            let user = await userDb.getUser(body.username);
+            if (!user) {
+                ctx.body = {error: 'no_user' };
+                return;
+            }
+
             user.password = newPass;
             await user.save();
+
+        } else if (body.act === 'newuser') {
+            let username = (body.username || '').trim();
+            let password = (body.password || '').trim();
+            let admin = (body.admin || '').trim();
+
+            if (!username || !password) {
+                ctx.body = {error: 'missing_params'};
+                return;
+            }
+
+            let existingUser = await userDb.getUser(username);
+            if (existingUser) {
+                ctx.body = {error: 'user_exists' };
+                return;
+            }
+    
+            try {
+                await userDb.addUser(username, password, admin === 'true');
+            } catch (err) {
+                l.error('[webui] Error adding new user:', err.message);
+                ctx.body = {error: 'unknown_error' };
+                return;
+            }
         }
 
         ctx.body = { error: false };
