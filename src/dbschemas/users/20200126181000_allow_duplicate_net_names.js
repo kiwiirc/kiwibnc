0f22d29d7a8dfb74babb125e9a35695b5567a5de
Darren <darren@darrenwhitlen.com>
2020-01-26 20:29:51 +0000

    Revert "Support networks with the same names"
    This reverts commit 9ba76757f5cc220d3cd22a62561fc05ad61a914c.

--- src/dbschemas/users/20200126181000_allow_duplicate_net_names.js
+++
@@ -1,9 +0,0 @@
-exports.up = async function(knex) {
-    await knex.schema.table('user_networks', table => {
-        table.dropUnique('name_user_id');
-    });
-};
-
-exports.down = function(knex) {
-    // Never go backwards in the db
-};
