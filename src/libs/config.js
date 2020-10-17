0d8a64597988a2f841201439bbe8f967d7987ea0
Darren <darren@darrenwhitlen.com>
2020-10-17 23:03:08 +0100

    Read overriding env vars from config relative .env file

--- src/libs/config.js
+++ src/libs/config.js
@@ -12,6 +12,7 @@ module.exports = class Config extends EventEmitter {
         this.filePath = filePath;
         this.baseDir = path.resolve(path.dirname(filePath));
         this.c = {};
+        this.env = {};
     }
 
     relativePath(pathInp) {
@@ -24,8 +25,16 @@ module.exports = class Config extends EventEmitter {
     }
 
     load() {
-        let confContent = fs.readFileSync(this.filePath);
-        let confObj = toml.parse(confContent.toString());
+        let confContent = fs.readFileSync(this.filePath).toString();
+        // Load a .env file of overriding environment vars
+        this.loadDotFile();
+
+        // Replace $ENV_VAR with the environment var VAR in the config file
+        confContent = confContent.replace(/\$ENV_([a-zA-Z0-9_]+)?/gm, (m, varName) => {
+            return this.env[varName] || process.env[varName] || '';
+        });
+
+        let confObj = toml.parse(confContent);
 
         this.emit('loaded', confObj);
         this.c = confObj;
@@ -33,17 +42,7 @@ module.exports = class Config extends EventEmitter {
     }
 
     get(key, def) {
-        let val = process.env['BNC_' + key.toUpperCase().replace(/\./g, '_')];
-        if (typeof val !== 'undefined') {
-            // If the value looks to be a JSON structure, parse it
-            if (val[0] === '[' || val[0] === '{' || val[0] === '"') {
-                val = JSON.parse(val);
-            }
-
-            return val;
-        }
-
-        val = _.get(this.c, key);
+        let val = _.get(this.c, key);
         if (typeof val === 'object') {
             // Replace any property values if there is an environment var overriding it
             for (let prop in val) {
@@ -64,6 +63,39 @@ module.exports = class Config extends EventEmitter {
         }
 
         return def;
+    }
+
+    loadDotFile(dotfile) {
+        let content = '';
+        let filename = dotfile || process.env.DOTFILE || '.env';
+        if (!filename) {
+            return;
+        }
+
+        try  {
+            content = fs.readFileSync(this.relativePath(filename), 'utf8');
+        } catch (err) {
+            return;
+        }
+
+        let lines = content.split('\n')
+            .map(line => line.trim())
+            .filter(line => !!line);
+
+        lines.forEach(line => {
+            let pos = line.indexOf('=');
+            if (pos === -1) {
+                return;
+            }
+
+            let key = line.substr(0, pos).trim();
+            let val = '';
+            for (let i=pos+1; i<line.length;i++) {
+                val += line[i];
+            }
+
+            this.env[key] = val.trim();
+        });
     }
 
     set(key, val) {
