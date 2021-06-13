9aa40aa0c348d7fe6beb5cc3f844ff03b39ee435
Darren Whitlen <darren@darrenwhitlen.com>
2021-06-13 18:00:52 +0100

    Correctly set lastSeen in bouncer CHANGEBUFFER

--- src/extensions/bouncer/index.js
+++ src/extensions/bouncer/index.js
@@ -250,7 +250,7 @@ async function handleBouncerCommand(event) {
                 new Date(tags.seen).getTime();
 
             if (!isNaN(seen)) {
-                buffer.lastSeen[con.clientid] = seen;
+                buffer.lastSeen[con.state.clientid] = seen;
             }
         }
 
